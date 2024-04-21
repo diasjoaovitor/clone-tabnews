a92637941776fb7a4a651c916e38e320b563b510
João Vitor <jvitordiass@outlook.com.br>
2024-04-21 19:03:27 -0300

    test: `status` endpoint

--- src/tests/integration/api/v1/status/get.test.ts
+++ src/tests/integration/api/v1/status/get.test.ts
@@ -1,10 +1,26 @@
-import { database } from '@/infra/database'
+import { TStatusBody } from '@/app/api/v1/status/route'
 
 describe('GET to /api/v1/status', () => {
   test('should return 200', async () => {
-    const result = await database.query('SELECT 1 + 1;')
     const response = await fetch('http://localhost:3000/api/v1/status')
-    expect(result).toBeDefined()
     expect(response.status).toBe(200)
+
+    const data: TStatusBody = await response.json()
+    const { updated_at } = data
+
+    const parsedUpdatedAt = new Date(updated_at).toISOString()
+    expect(updated_at).toBe(parsedUpdatedAt)
+
+    const expectedData: TStatusBody = {
+      updated_at,
+      dependencies: {
+        database: {
+          version: '16.0',
+          max_connections: '100',
+          opened_connections: 1
+        }
+      }
+    }
+    expect(data).toEqual(expectedData)
   })
 })
