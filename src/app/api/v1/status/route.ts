a0b2c62c7ca212d82b0f42dc5ddabf16020d4800
João Vitor <jvitordiass@outlook.com.br>
2024-04-21 19:02:21 -0300

    feat: implements `status` endpoint

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,12 +1,52 @@
 import { NextResponse } from 'next/server'
+import { QueryResult, QueryResultRow } from 'pg'
 import { database } from '@/infra/database'
 
-export const GET = async () => {
-  const result = await database.query('SELECT 1 + 1;')
-  return NextResponse.json(
-    { message: result },
-    {
-      status: 200
+export type TStatusBody = {
+  updated_at: string
+  dependencies: {
+    database: {
+      version: string
+      max_connections: string
+      opened_connections: number
     }
-  )
+  }
+}
+
+export const GET = async () => {
+  const updatedAt = new Date().toISOString()
+
+  const {
+    rows: [{ server_version }]
+  } = (await database.query(
+    'show server_version;'
+  )) as QueryResult<QueryResultRow>
+
+  const {
+    rows: [{ max_connections }]
+  } = (await database.query(
+    'show max_connections;'
+  )) as QueryResult<QueryResultRow>
+
+  const databaseName = process.env.POSTGRES_DB as string
+  const {
+    rows: [{ count }]
+  } = (await database.query({
+    text: 'select count(*)::int from pg_stat_activity where datname = $1;',
+    values: [databaseName]
+  })) as QueryResult<QueryResultRow>
+
+  const body: TStatusBody = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: server_version,
+        max_connections,
+        opened_connections: count
+      }
+    }
+  }
+  return NextResponse.json(body, {
+    status: 200
+  })
 }
