83b1c9256a1ffc9c3b0baeae98e54f758d721098
João Vitor <jvitordiass@outlook.com>
2024-11-05 23:06:41 -0300

    test: add tests for not allowed methods in migrations

---
+++ src/tests/integration/api/v1/migrations/not-allowed.test.ts
@@ -0,0 +1,44 @@
+import { cleanDatabase } from '@/tests/orchestrator'
+
+const url = 'http://localhost:3000/api/v1/migrations'
+
+describe('Not allowed methods to /api/v1/migrations', () => {
+  beforeAll(cleanDatabase)
+
+  test('should return 405 and the response error', async () => {
+    const response1 = await fetch(url, {
+      method: 'PUT'
+    })
+    expect(response1.status).toBe(405)
+    expect(await response1.json()).toEqual({ error: 'Method PUT not allowed' })
+
+    const response2 = await fetch(url, {
+      method: 'DELETE'
+    })
+    expect(response2.status).toBe(405)
+    expect(await response2.json()).toEqual({
+      error: 'Method DELETE not allowed'
+    })
+
+    const response3 = await fetch(url, {
+      method: 'PATCH'
+    })
+    expect(response3.status).toBe(405)
+    expect(await response3.json()).toEqual({
+      error: 'Method PATCH not allowed'
+    })
+
+    const response4 = await fetch(url, {
+      method: 'OPTIONS'
+    })
+    expect(response4.status).toBe(405)
+    expect(await response4.json()).toEqual({
+      error: 'Method OPTIONS not allowed'
+    })
+
+    const response5 = await fetch(url, {
+      method: 'HEAD'
+    })
+    expect(response5.status).toBe(405)
+  })
+})
