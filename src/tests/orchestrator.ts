e4f56ba25b6ee773d2485d3529d441904d6887bc
João Vitor <jvitordiass@outlook.com>
2024-11-28 15:36:04 -0300

    fix: eslint linting errors

--- src/tests/orchestrator.ts
+++ src/tests/orchestrator.ts
@@ -2,13 +2,19 @@ import retry from 'async-retry'
 import database from '@/infra'
 
 const waitForAllServices = async () => {
-  await retry(async () => {
-    const response = await fetch('http://localhost:3000/api/v1/status')
-    if (!response.ok) {
-      throw new Error('Service not ready')
+  await retry(
+    async () => {
+      const response = await fetch('http://localhost:3000/api/v1/status')
+      if (!response.ok) {
+        throw new Error('Service not ready')
+      }
+    },
+    {
+      retries: 60,
+      maxTimeout: 1000,
+      minTimeout: 100
     }
-  }),
-    { retries: 60, maxTimeout: 1000, minTimeout: 100 }
+  )
 }
 
 const clearDatabase = async () => {
