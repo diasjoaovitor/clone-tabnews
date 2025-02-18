7e63a0794c8abb6c489faf6e110cae0f33275db2
João Vitor <jvitordiass@outlook.com>
2025-02-18 14:55:47 -0300

    🧪 test: improve test descriptions and structure for api endpoints

--- src/tests/integration/api/v1/status/get.test.ts
+++ src/tests/integration/api/v1/status/get.test.ts
@@ -4,26 +4,28 @@ import { TStatusBody } from '@/app/api/v1/status/route'
 beforeAll(orchestrator.waitForAllServices)
 
 describe('GET to /api/v1/status', () => {
-  test('should return 200', async () => {
-    const response = await fetch('http://localhost:3000/api/v1/status')
-    expect(response.status).toBe(200)
+  describe('Anonymous user', () => {
+    test('Retrieving current system status', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/status')
+      expect(response.status).toBe(200)
 
-    const data: TStatusBody = await response.json()
-    const { updated_at } = data
+      const data: TStatusBody = await response.json()
+      const { updated_at } = data
 
-    const parsedUpdatedAt = new Date(updated_at).toISOString()
-    expect(updated_at).toBe(parsedUpdatedAt)
+      const parsedUpdatedAt = new Date(updated_at).toISOString()
+      expect(updated_at).toBe(parsedUpdatedAt)
 
-    const expectedData: TStatusBody = {
-      updated_at,
-      dependencies: {
-        database: {
-          version: '16.0',
-          max_connections: '100',
-          opened_connections: 1
+      const expectedData: TStatusBody = {
+        updated_at,
+        dependencies: {
+          database: {
+            version: '16.0',
+            max_connections: '100',
+            opened_connections: 1
+          }
         }
       }
-    }
-    expect(data).toEqual(expectedData)
+      expect(data).toEqual(expectedData)
+    })
   })
 })
