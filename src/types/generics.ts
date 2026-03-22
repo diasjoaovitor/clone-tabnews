c3f42d41172610f520709129951532191e01ef93
João Vitor <jvitordiass@outlook.com>
2026-03-22 19:16:13 -0300

    ✨ feat: enhance generics with new type utilities
    - Add TStringToDate type to convert string types to Date.
    - Introduce TGetDto type for creating Data Transfer Objects with user
      session context.
    - Import TUserSession for improved type safety in DTOs.
    
    Issue: #14

--- src/types/generics.ts
+++ src/types/generics.ts
@@ -1,3 +1,5 @@
+import { TUserSession } from '@/infra'
+
 type TAutomaticFields = 'id' | 'created_at' | 'updated_at'
 
 export type TDataInput<T> = T extends readonly (infer U)[]
@@ -10,6 +12,14 @@ type TDateToString<T> = T extends Date
     ? { [K in keyof T]: TDateToString<T[K]> }
     : T
 
+export type TStringToDate<T> = T extends string
+  ? Date
+  : T extends object
+    ? { [K in keyof T]: TStringToDate<T[K]> }
+    : T
+
 export type TApiResponse<T> = T extends readonly (infer U)[]
   ? TDateToString<U>[]
   : TDateToString<T>
+
+export type TGetDto<T, R> = (user: TUserSession | null, data: T) => R
